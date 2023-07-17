1ef4fcfb80539a90638923dafd4e2802d8e1a36c
Sonnydiscord <139786189+Sonnydiscord@users.noreply.github.com>
2023-07-17 19:10:29 +0200

    Update buyer.js

--- commands/bot gestion/buyer.js
+++ commands/bot gestion/buyer.js
@@ -19,51 +19,6 @@ module.exports = {
 
     run: async (client, message, args, color, prefix, footer, commandName) => {
 
+        // A REFAIRE
 
-    request(`http://localhost:3000/getbuyer/${client.user.id}`, (err, res, body) => {
-        let data = JSON.parse(body).body
-        if(data !== message.author.id) return message.reply("Vous n'êtes pas l'acheteur de ce bot")
-        let list = args[0] === "list"
-        let add = args[0] === "add"
-        let remove = args[0] === "remove"
-    
-        if (add) {
-            let buyer = message.mentions.users.first()
-            if (!buyer) return message.reply("Veuillez mentionner un utilisateur")
-    
-            let config = client.config
-            let buyers = config.buyers
-    
-            if (buyers.includes(buyer.id)) return message.reply("Cet utilisateur est déjà dans la liste des buyers")
-    
-            buyers.push(buyer.id)
-            client.config.buyers = buyers
-    
-            fs.writeFile("./config.json", JSON.stringify(client.config, null, 4), err => {
-                if (err) throw err
-                message.reply("L'utilisateur a bien été ajouté à la liste des buyers")
-            })
-        }
-    
-        if (remove) {
-            let buyer = message.mentions.users.first()
-            if (!buyer) return message.reply("Veuillez mentionner un utilisateur")
-    
-            let config = client.config
-            let buyers = config.buyers
-    
-            if (!buyers.includes(buyer.id)) return message.reply("Cet utilisateur n'est pas dans la liste des buyers")
-    
-            buyers.splice(buyers.indexOf(buyer.id), 1)
-            client.config.buyers = buyers
-    
-            fs.writeFile("./config.json", JSON.stringify(client.config, null, 4), err => {
-                if (err) throw err
-                message.reply("L'utilisateur a bien été retiré de la liste des buyers")
-            })
-        }
-    })
-
-
-}
-}
+    }}
